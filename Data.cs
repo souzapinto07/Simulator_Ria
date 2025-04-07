f0a86c0718af7ef6a11d56e021b62ed3ef587317
Lucas Pinto <souzapinto07@gmail.com>
2025-04-07 14:16:29 -0300

    Change Data To static

--- Data.cs
+++ Data.cs
@@ -7,20 +7,20 @@ using System.Threading.Tasks;
 
 namespace Simulator_Ria
 {
-    public class Data
+    public static class Data
     {
         private const int MIN_CUSTOMERS_PER_REQUEST = 2;
         private const int MAX_CUSTOMERS_PER_REQUEST = 5;
         private const int MIN_AGE = 10;
         private const int MAX_AGE = 90;
-        private int _idIncremental = 0;
-        private Random _random = new Random();
+        private static int _idIncremental = 0;
+        private static Random _random = new Random();
 
 
-        private string[] FIRST_NAMES = new string[] { "Aarav", "Yuki", "Amina", "Nikolai", "Mei", "Jakub", "Leila", "Rafael", "Priya", "Elijah", "Sakura", "Mohammed", "Ananya", "Gabriel", "Zeynep" };
-        private string[] LAST_NAMES = new string[] { "Wang", "Kim", "Nguyen", "Patel", "Silva", "Ivanov", "Kovács", "Müller", "Santos", "Cohen", "Saito", "Khan", "Nowak", "Dubois" };
+        private static string[] FIRST_NAMES = new string[] { "Aarav", "Yuki", "Amina", "Nikolai", "Mei", "Jakub", "Leila", "Rafael", "Priya", "Elijah", "Sakura", "Mohammed", "Ananya", "Gabriel", "Zeynep" };
+        private static string[] LAST_NAMES = new string[] { "Wang", "Kim", "Nguyen", "Patel", "Silva", "Ivanov", "Kovács", "Müller", "Santos", "Cohen", "Saito", "Khan", "Nowak", "Dubois" };
 
-        public List<Customer> GenerateRandomCustomers()
+        public static List<Customer> GenerateRandomCustomers()
         {
 
             int count = _random.Next(MIN_CUSTOMERS_PER_REQUEST, MAX_CUSTOMERS_PER_REQUEST + 1);
